699740cfbab9163c3f81fbff827d71abf0065915
Bibekad24 <166050249+Bibekad24@users.noreply.github.com>
2024-05-27 15:28:46 -0700

    Updated Expense.java

--- src/MoneyMate/Expense.java
+++ src/MoneyMate/Expense.java
@@ -8,12 +8,12 @@ public class Expense {
 
     /**
      * Constructs a new Expense.
+     * @param id         the ID of the expense.
      * @param date       the date of the expense.
      * @param category   the category of the expense.
      * @param amount     the amount of the expense.
-     *
-     * */
-    public Expense(String date, String category, double amount) {
+     */
+    public Expense(int id, String date, String category, double amount) {
         this.id = id;
         this.date = date;
         this.category = category;
@@ -24,7 +24,7 @@ public class Expense {
      * Gets the id of the expense.
      *
      * @return the id of the expense.
-     * */
+     */
     public int getId() {
         return id;
     }
@@ -33,7 +33,7 @@ public class Expense {
      * Gets the date of the expense.
      *
      * @return the date of the expense.
-     * */
+     */
     public String getDate() {
         return date;
     }
@@ -42,19 +42,17 @@ public class Expense {
      * Gets the category of the expense.
      *
      * @return the category of expense.
-     * */
+     */
     public String getCategory() {
         return category;
     }
-
 
     /**
      * Gets the amount of the expense.
      *
      * @return the amount of the expense.
-     * */
+     */
     public double getAmount() {
         return amount;
     }
 }
-
